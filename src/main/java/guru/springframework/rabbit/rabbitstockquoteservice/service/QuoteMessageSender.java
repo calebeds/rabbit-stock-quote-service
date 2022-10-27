2cfc69a1e0735dda0caf091575fcee71d5eaff08
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-27 12:20:48 -0300

    implmement receiver

--- src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteMessageSender.java
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteMessageSender.java
@@ -5,12 +5,17 @@ import guru.springframework.rabbit.rabbitstockquoteservice.config.RabbitConfig;
 import guru.springframework.rabbit.rabbitstockquoteservice.model.Quote;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.rabbitmq.OutboundMessage;
+import reactor.rabbitmq.OutboundMessageResult;
+import reactor.rabbitmq.QueueSpecification;
 import reactor.rabbitmq.Sender;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class QuoteMessageSender {
     private final ObjectMapper objectMapper;
@@ -20,6 +25,18 @@ public class QuoteMessageSender {
     public Mono<Void> sendQuoteMessage(Quote quote) {
         byte[] jsonBytes = objectMapper.writeValueAsBytes(quote);
 
-        return sender.send(Mono.just(new OutboundMessage("", RabbitConfig.QUEUE, jsonBytes)));
+        Flux<OutboundMessageResult> confirmations = sender.sendWithPublishConfirms(
+                Flux.just(new OutboundMessage("", RabbitConfig.QUEUE, jsonBytes)));
+
+        sender.declareQueue(QueueSpecification.queue(RabbitConfig.QUEUE))
+                .thenMany(confirmations)
+                .doOnError(e -> log.error("Send failed", e))
+                .subscribe(r -> {
+                    if(r.isAck()){
+                        log.info("Message sent successfully {}", new String(r.getOutboundMessage().getBody()));
+                    }
+                });
+
+        return Mono.empty();
     }
 }
